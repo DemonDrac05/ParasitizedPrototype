583b76aa6ef221f76ebe1495faa0c7b4b19f7af0
DualGaming4 <lazybonespunmaster@gmail.com>
2025-02-05 17:07:53 +0200

    Player No Longer Jumps Infinite Times

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,7 +12,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
-    // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
